255f93b9c1504be926238fac71fd58ad186d3fe9
RJVD20 <robinjulian9@gmail.com>
2021-01-28 11:05:57 +0100

    index

--- index.php
+++ index.php
@@ -9,6 +9,20 @@
     lalalala
 </body>
 </html>
+
 <?php
-    SELECT * FROM `news`
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $db = "apotheek";
+    $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+    
+    return $conn;
+    
+  
+function CloseCon($conn)
+{
+$conn -> close();
+}
+  
 ?>
