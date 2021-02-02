dcdfad7a3b8309e132d4814191e15ee949cdb62e
Serge <serge.klercq@xs4all.nl>
2021-02-02 09:32:09 +0100

    html

--- medicijnoverzicht.php
+++ medicijnoverzicht.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../css/commonstyle.css">
+    <link rel="stylesheet" href="css/commonstyle.css">
 </head>
 <body>
+<h2>menu</h2>
     <div class="nav">
-        <a href="home.html">Home</a>
-        <a href="voorlichting.html">Voorlichting</a>
-        <a href="inlogscherm.html">Inloggen</a>
-        <a href="service.html">Service</a>
+    <a href="index.php">Home</a>
+        <a href="voorlichting.php">Voorlichting</a>
+        <a href="inlogscherm.php">Inloggen</a>
+        <a href="service.php">Service</a>
     </div>
 
     <div class="mid">
