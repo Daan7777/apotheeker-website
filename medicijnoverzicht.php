3919901f7e63039f22e278bcf59869dab5fa7052
RJVD20 <robinjulian9@gmail.com>
2021-02-02 11:25:34 +0100

    query

--- medicijnoverzicht.php
+++ medicijnoverzicht.php
@@ -6,7 +6,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="css/commonstyle.css">
 </head>
-<body>
+<body> 
 <h2>menu</h2>
     <div class="nav">
     <a href="index.php">Home</a>
@@ -14,7 +14,38 @@
         <a href="inlogscherm.php">Inloggen</a>
         <a href="service.php">Service</a>
     </div>
-
+        </body>
     <div class="mid">
         </h2>
     </div>
+
+    <?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$db = "apotheek";
+
+// Create connection
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+        $sql = "SELECT medicineid, medicinename  FROM medicines";
+        $result = $conn->query($sql);
+        
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "<br> id: ". $row["medicineid"]. " - Name: ". $row["medicinename"]. "<br>";
+            }
+        } else {
+            echo "BIG FAT error";
+        }
+
+        {
+            $conn -> close();
+            }
+
+    ?>
