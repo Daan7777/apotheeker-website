22f0dd573e740ebe565c5a31b6c6d247e38c1852
RJVD20 <robinjulian9@gmail.com>
2021-02-04 22:46:21 +0100

    mail fix

--- connect.php
+++ connect.php
@@ -4,17 +4,18 @@
     
        $Firstname = $_POST['firstname'];
        $Lastname = $_POST['lastname'];
+       $mail = $_POST['mail'];
        $Country = $_POST['country'];
        $Subject = $_POST['subject'];
 
-       if(empty($Firstname) || empty($Lastname) || empty($Country) || empty($Subject))
+       if(empty($Firstname) || empty($Lastname) || empty($mail) || empty($Country) || empty($Subject))
        {
            header('location:inschrijven.php?error');
        }
        else
        {}
     // Hier komt de code om de data uit de formulier naar de database te sturen
-    $sql = "INSERT INTO `user` (`id`, `firstname`, `lastname`, `mail`, `country`, `subject`) VALUES (NULL, '$Firstname', '$Lastname', '$mail', '$Country', '$Subject') ";
+    $sql = "INSERT INTO `user` (`id`, `firstname`, `lastname`, `mail` , `country`, `subject`) VALUES (NULL, '$Firstname', '$Lastname', '$mail', '$Country', '$Subject') ";
     $result = $conn->query($sql);{
 
         header('location:inschrijven.php?Hetisgelukt');
