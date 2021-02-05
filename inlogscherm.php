353cedf955a3d075089388bae40505adfaf290db
RJVD20 <robinjulian9@gmail.com>
2021-02-05 11:29:43 +0100

    inlog html css

--- inlogscherm.php
+++ inlogscherm.php
@@ -7,15 +7,31 @@
     <link rel="stylesheet" href="css/commonstyle.css">
 </head>
 <body> 
-<img class="logo" src="pictures/banner.jpg">
-    <div class="menu">    <h2>menu</h2> </div> <br>
-        <div class="nav">
-        <a href="index.php">Home</a>
-        <a href="voorlichting.php">Voorlichting</a>
-        <a href="inlogscherm.php">Inloggen</a>
-        <a href="service.php">Service</a>
-    </div> <br>
+    </div>
         </body>
     <div class="mid">
         </h2>
     </div>
+    <form action="/action_page.php" method="post">
+  <div class="imgcontainer">
+    <img src="pictures/img_avatar2.png" alt="Avatar" class="avatar">
+  </div>
+
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+        
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
