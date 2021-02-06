a6de992367270917c32f20dd8b4639545c815603
Serge <serge.klercq@xs4all.nl>
2021-02-06 13:06:56 +0100

    alles gefixt

--- service.php
+++ service.php
@@ -8,7 +8,7 @@
 </head>
 <body> 
 <img class="logo" src="pictures/banner.jpg">
-    <div class="menu">    <h2>menu</h2> </div> <br>
+    <div class="menu">    <h2>menu</h2> </div> 
         <div class="nav">
         <a href="index.php">Home</a>
         <a href="voorlichting.php">Voorlichting</a>
