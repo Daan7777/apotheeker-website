d8ea67b8221aa6aa716dd355558d5f040b9fe9cc
RJVD20 <robinjulian9@gmail.com>
2021-02-08 09:36:34 +0100

    lalala

--- inlogscherm.php
+++ inlogscherm.php
@@ -45,9 +45,29 @@
     <span class="psw">Forgot <a href="#">password?</a></span>
   </div>
 </form>
-
-
 <?php
     include("includes/databaselink.php");
     
+    mysql_connect($host,$user,$password);
+mysql_select_db($db);
+
+if(isset($_POST['username'])){
+    
+    $uname=$_POST['username'];
+    $password=$_POST['password'];
+    
+    $sql="select * from loginform where user='".$uname."'AND Pass='".$password."' limit 1";
+    
+    $result=mysql_query($sql);
+    
+    if(mysql_num_rows($result)==1){
+        echo " You Have Successfully Logged in";
+        exit();
+    }
+    else{
+        echo " You Have Entered Incorrect Password";
+        exit();
+    }
+        
+}
   ?>
