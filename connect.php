abed8373a84adc327695e3a23362399a157396be
RJVD20 <robinjulian9@gmail.com>
2021-02-16 09:11:36 +0100

    lala

--- connect.php
+++ connect.php
@@ -24,33 +24,4 @@
     
        }
 
-
-
-       $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
-if (mysqli_connect_error()){
-die('Connect Error ('. mysqli_connect_errno() .') '
-. mysqli_connect_error());
-}
-else{
-$sql = "INSERT INTO account (username, password)
-values ('$username','$password')";
-if ($conn->query($sql)){
-echo "New record is inserted sucessfully";
-}
-else{
-echo "Error: ". $sql ."
-". $conn->error;
-}
-$conn->close();
-}
-}
-else{
-echo "Password should not be empty";
-die();
-}
-}
-else{
-echo "Username should not be empty";
-die();
-}
 ?>
